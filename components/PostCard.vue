26ec17f4093a14ec44538a173d2039817e28886b
Steve McDougall <juststeveking@gmail.com>
2020-02-14 13:27:47 +0000

    Extending tailwind colours and working on post card component styling

--- components/PostCard.vue
+++ components/PostCard.vue
@@ -1,12 +1,20 @@
 <template>
-  <article>
-    <header>
-      <nuxt-link
-        :to="`/posts/${post.id}`"
-      >
-        <h3>{{ post.title }}</h3>
-      </nuxt-link>
-    </header>
+  <article class="w-full lg:w-1/3 md:mx-2 mb-4 md:mb-0">
+    <div class="bg-white rounded-lg overflow-hidden shadow relative">
+      <img class="h-56 w-full object-cover object-center" :src="post.image" :alt="post.title">
+      <header class="p-4 h-auto md:h-40 lg:h-48">
+        <nuxt-link
+          :to="`/posts/${post.id}`"
+        >
+          <h3 class="block text-blue-500 hover:text-blue-600 font-semibold mb-2 text-lg md:text-base lg:text-lg">
+            {{ post.title }}
+          </h3>
+        </nuxt-link>
+      </header>
+      <div class="text-gray-600 text-sm leading-relaxed block md:text-xs lg:text-sm">
+        {{ post.content }}
+      </div>
+    </div>
   </article>
 </template>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,29 @@
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        red: '#d4351c',
+        yellow: '#ffdd00',
+        green: '#00703c',
+        blue: '#1d70b8',
+        'dark-blue': '#003078',
+        'light-blue': "#5694ca",
+        purple: '#4c2c92',
+        'light-purple': '#6f72af',
+        'bright-purple': '#912b88',
+        black: '#0b0c0c',
+        'dark-grey': '#626a6e',
+        'mid-grey': '#b1b4b6',
+        'light-grey': '#f3f2f1',
+        white: '#ffffff',
+        pink: '#d53880',
+        'light-pink': '#f499be',
+        'orange': '#f47738',
+        brown: '#b58840',
+        'light-green': '#85994b',
+        turquoise: '#28a197'
+      }
+    },
   },
   variants: {},
   plugins: [],
